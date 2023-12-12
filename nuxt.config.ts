5c07affc8d66c4c4d1ab474d45a0fd3d71331000
niicez <tuandaris1@gmail.com>
2023-12-13 03:49:24 +0700

    feat: eveything to 'src' folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: ['@nuxtjs/tailwindcss'],
-    tailwindcss: {}
+    tailwindcss: {},
+    srcDir: 'src/'
 })
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { data } = await useFetch('/api/hello')
+</script>
+
+<template>
+    <div>
+        <p>{{ data }}</p>
+        <h1 class="text-center">Hello World!</h1>
+    </div>
+</template>
